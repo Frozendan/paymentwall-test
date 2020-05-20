9d5149237c7c41e75a03dab02c9898e34d7f59e3
anhtran <anhtt1904@gmail.com>
2020-05-21 02:58:08 +0700

    finish form

--- src/app/core/payment.service.ts
+++ src/app/core/payment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { Country } from '../core/models/country.model';
+import { AbstractControl } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -16,19 +17,20 @@ export class PaymentService {
   API_ENDPOINT = 'https://api.paymentwall.com/api/payment-systems/';
   COUNTRY_CODE = 'VN';
   API_COUNTRY_CODE = 'https://api.paymentwall.com/api/rest/country';
+  API_ALL_COUNTRY_CODES = 'https://restcountries.eu/rest/v2/all';
 
   constructor(private http: HttpClient) { }
 
   getCountryList() {
-    return this.http.get<Country[]>(this.API_ALL_COUNTRY_CODES)
+    return this.http.get(this.API_ALL_COUNTRY_CODES)
       .pipe(
         retry(1),
         catchError(this.handleError)
       );
   }
 
-  getCountry() {
-    return this.http.get<Country[]>(this.API_COUNTRY_CODE + `?uid=${this.SECRET_KEY}`)
+  getCountryByGeolocation() {
+    return this.http.get<Country>(this.API_COUNTRY_CODE + `?uid=${this.SECRET_KEY}`)
       .pipe(
         retry(1),
         catchError(this.handleError)
@@ -63,3 +65,35 @@ export class PaymentService {
     return throwError(errorMessage);
   }
 }
+
+// Takes a credit card string value and returns true on valid number
+export function validateCardNumber(control: AbstractControl): { [key: string]: any } | null {
+// Accept only digits, dashes or spaces
+  if (/[^0-9-\s]+/.test(control.value)) { return { incorrect: true }; }
+
+  // The Luhn Algorithm.
+  let nCheck = 0, bEven = false;
+  const value = control.value.replace(/\D/g, '');
+
+  for (let n = value.length - 1; n >= 0; n--) {
+    let cDigit = value.charAt(n), nDigit = parseInt(cDigit, 10);
+
+    if (bEven && (nDigit *= 2) > 9) { nDigit -= 9; }
+
+    nCheck += nDigit;
+    bEven = !bEven;
+  }
+  return (nCheck % 10) == 0 ? null : { incorrect: true };
+}
+
+export function validateExpiredDate(control: AbstractControl): { [key: string]: any } | null {
+  const date = new Date();
+  if (!/^\d{2}\/\d{2}$/g.test(control.value)) { return { incorrect: true }; }
+  console.log(control.value);
+  let [ month, year ] = control.value.split('/');
+  year = 20 + year;
+  const expiryDate = new Date(year, month);
+  return expiryDate > date ? null : { incorrect: true };
+}
+
+
--- src/app/features/payment-form/payment-form.component.ts
+++ src/app/features/payment-form/payment-form.component.ts
@@ -1,32 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import {PaymentService} from '../../core/payment.service';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {PaymentService, validateCardNumber, validateExpiredDate} from '../../core/payment.service';
 import { Country } from '../../core/models/country.model';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-payment-form',
   templateUrl: './payment-form.component.html',
   styleUrls: ['./payment-form.component.scss']
 })
 export class PaymentFormComponent implements OnInit {
-  countries: Country[];
-  paymentMethod: {};
+  @ViewChild('content')
+  content: ElementRef;
+
+  countries: any = [];
+  paymentMethod: any = [];
   paymentForm: FormGroup;
+  currentCurrency: '';
   constructor(private paymentService: PaymentService,
-              private fb: FormBuilder) {
-    this.showPayment();
-    this.getListCountry();
+              private fb: FormBuilder,
+              private modalService: NgbModal) {
   }
 
   ngOnInit(): void {
     this.paymentForm = this.fb.group({
-      // amount: ['', Validators.required],
       country: ['', Validators.required],
-      // paymentMethod: ['', Validators.required],
-      // cardName: ['', Validators.required],
-      // cardNumber: ['', [Validators.required, cardNumberValidator]],
-      // cardExp: ['', [Validators.required, cardExpDateValidator]],
-      // cardCVV: ['', Validators.required],
+      amount: ['', [Validators.required]],
+      paymentMethod: ['', Validators.required],
+      cardholderName: ['', Validators.required],
+      creditCardNumber: ['', [Validators.required, validateCardNumber]],
+      expDate: ['', [Validators.required, validateExpiredDate]],
+      cvv: ['', Validators.required],
     });
+    this.initListCountry();
+    this.showPayment();
+    this.paymentForm.get('country').valueChanges.subscribe(data => {
+      this.getCurrency(data);
+    });
+  }
+
+  async initListCountry() {
+    try {
+      this.countries = await this.paymentService.getCountryList().toPromise();
+      const currCountry: any = await this.paymentService.getCountryByGeolocation().toPromise();
+      this.paymentForm.patchValue({ country: currCountry.code });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  getCurrency(code){
+    const filteredCountry: any = this.countries.filter((country: any) => country.alpha2Code === code);
+    this.currentCurrency = filteredCountry[0].currencies[0].code;
   }
 
   showPayment() {
@@ -36,15 +60,19 @@ export class PaymentFormComponent implements OnInit {
     });
   }
 
-  getListCountry() {
-    this.paymentService.getCountryList().subscribe(data => {
-      this.countries = data;
-      console.log(data)
+  openModal(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      console.log(result);
+    }, (reason) => {
+      console.log(reason);
     });
   }
 
   onSubmit(){
-    console.log(123);
+    if (this.paymentForm.invalid) {
+      return;
+    }
+    this.openModal(this.content);
   }
 
 }
